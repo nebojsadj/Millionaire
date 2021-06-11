4fa83983e2f170a300c49ecd6de69af3924283ac
Nebojsadj <djordjevicn87@gmail.com>
2021-06-11 13:17:55 +0200

    change key name

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,62 +2,62 @@ export const questions = [
   {
     question: "U prevodu na srpski, internacionalno je...",
     options: [`dobrostojece`, "medjunarodno", "niskoletece", "maloprodajno"],
-    answer: "medjunarodno",
+    correct: "medjunarodno",
     points: 15,
   },
   {
     question: "Kakva obavezno mora biti mini suknja?",
     options: [`zelena`, "duga", "kratka", "vunena"],
-    answer: "kratka",
+    correct: "kratka",
     points: 25,
   },
   {
     question: "Gledano iz Srbije, Skandinavija je?",
     options: [`na jugu`, "pod zemljom", "na severu", "pod morem"],
-    answer: "na severu",
+    correct: "na severu",
     points: 35,
   },
   {
     question: "Poznata izreka veli - Proveo se kao bos...",
     options: [`na krovu`, "u vojsci", "bez sokni", "po trnju"],
-    answer: "po trnju",
+    correct: "po trnju",
     points: 45,
   },
   {
     question: "Koji naucnik je bio osnivac psihoanalize?",
     options: [`Nils Bor`, "Carls Darvin", "Sigmund Frojd", "Luj Paster"],
-    answer: "Sigmund Frojd",
+    correct: "Sigmund Frojd",
     points: 55,
   },
   {
     question: "Internet nam je omogucio da cetujemo, ili, u prevodu, da...",
     options: [`cestitamo`, "cavrljamo", "cerupamo", "ceprkamo"],
-    answer: "cavrljamo",
+    correct: "cavrljamo",
     points: 75,
   },
   {
     question:
       "Prema prici poznate serije stripova i filmova, Supermen postaje slabic u dodiru sa...",
     options: [`zlatom`, "kriptonitom", "vodom", "uranijumom"],
-    answer: "kriptonitom",
+    correct: "kriptonitom",
     points: 95,
   },
   {
     question: "Na kojoj reci lezi grad Pirot?",
     options: [`Timok`, "Juzna Morava", "Nisava", "Zapadna Morava"],
-    answer: "Nisava",
+    correct: "Nisava",
     points: 115,
   },
   {
     question: "Stara i vrlo popularna sorta jabuka je...",
     options: [`Viljamovka`, "Budimka", "Nektarina", "Pozegaca"],
-    answer: "Budimka",
+    correct: "Budimka",
     points: 135,
   },
   {
     question: "Otoskop prvenstveno sluzi za pregled...",
     options: [`oka`, "uha", "grla", "pluca"],
-    answer: "uha",
+    correct: "uha",
     points: 155,
   },
   {
@@ -68,35 +68,35 @@ export const questions = [
       "Zeljko Rebraca",
       "Ratko Varda",
     ],
-    answer: "Zarko Paspalj",
+    correct: "Zarko Paspalj",
     points: 200,
   },
   {
     question:
       "Italijanski privrednik i politicar Silvio Berluskoni vlasnik je koje kompanije?",
     options: [`Pirelli`, "Indesit", "Intesa Sanpaolo", "Mediaset"],
-    answer: "Mediaset",
+    correct: "Mediaset",
     points: 250,
   },
   {
     question:
       "Medju 22 karte velike arkane tarota nalazi se i ona ciji je naziv...",
     options: [`mlada zora`, "zlatni presto", "bojna kola", "stara voda"],
-    answer: "bojna kola",
+    correct: "bojna kola",
     points: 300,
   },
   {
     question:
       "Koji rimski istoricar je u delu Anali, izmedju ostalog opisao,kako je car Neron zapalio Rim?",
     options: [`Mucijan`, "Zosim", "Tacit", "Plutarh"],
-    answer: "Tacit",
+    correct: "Tacit",
     points: 400,
   },
   {
     question:
       "Koji od navedenih holivudskih glumaca je najcesce tumacio likove koji ne docekaju kraj filma?",
     options: [`Dzek Nikolson`, "Robert de Niro", "Dastin Hofman", "Al Pacino"],
-    answer: "Robert de Niro",
+    correct: "Robert de Niro",
     points: 500,
   },
 ];
