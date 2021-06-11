b1415f69071ca299f61af9ade8a074d1ef364064
Nebojsadj <djordjevicn87@gmail.com>
2021-06-11 15:37:48 +0200

    guaranteed amount and marking of the current amount

--- src/components/SecondaryMenu/SecondaryMenu.js
+++ src/components/SecondaryMenu/SecondaryMenu.js
@@ -1,27 +1,22 @@
 import React from "react";
 import "./Secondary.scss";
+import { money } from "../Money";
 import half from "../../images/img1.png";
 import friend from "../../images/img2.png";
 import audience from "../../images/img3.png";
 
-function SecondaryMenu({ points }) {
-  const money = [
-    { number: "15 ⋄", money: "3000000" },
-    { number: "14 ⋄", money: "1500000" },
-    { number: "13 ⋄", money: "750000" },
-    { number: "12 ⋄", money: "375000" },
-    { number: "11 ⋄", money: "192000" },
-    { number: "10 ⋄", money: "96000" },
-    { number: "9 ⋄", money: "48000" },
-    { number: "8 ⋄", money: "24000" },
-    { number: "7 ⋄", money: "12000" },
-    { number: "6 ⋄", money: "6000" },
-    { number: "5 ⋄", money: "3000" },
-    { number: "4 ⋄", money: "1500" },
-    { number: "3 ⋄", money: "900" },
-    { number: "2 ⋄", money: "600" },
-    { number: "1 ⋄", money: "300" },
-  ];
+function SecondaryMenu({ points, index }) {
+  const mark = (el) => {
+    if (index < 15) {
+      return money[index].id === el;
+    }
+  };
+
+  const guaranted = (el) => {
+    if (el === 5 || el === 10 || el === 15) {
+      return "guaranteed";
+    }
+  };
 
   return (
     <div id="secondary">
@@ -38,7 +33,12 @@ function SecondaryMenu({ points }) {
       </div>
       <div className="profit">
         {money.map((el) => (
-          <div className="money">{`${el.number} ${el.money}`}</div>
+          <div
+            className={`money ${guaranted(el.id)} ${
+              mark(el.id) && "markMoney"
+            }`}
+            key={el.id}
+          >{`${el.number} ${el.money}`}</div>
         ))}
         <hr />
         <div className="points">{points}</div>
